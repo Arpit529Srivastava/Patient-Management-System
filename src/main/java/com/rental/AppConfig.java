bb8ff193415dfea283e99b8ce6e31d48b0a54414
arpit529srivastava <arpitsrivastava529@gmail.com>
2026-05-17 09:16:09 +0530

    Fix: Add multiple Car beans in AppConfig for List<Car> injection

--- src/main/java/com/rental/AppConfig.java
+++ src/main/java/com/rental/AppConfig.java
@@ -1,5 +1,6 @@
 package com.rental;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,4 +9,25 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan(basePackages = "com.rental")
 @EnableAspectJAutoProxy
 public class AppConfig {
+
+    @Bean
+    public Car car1() {
+        Car car = new Car();
+        car.setModel("Toyota Camry");
+        return car;
+    }
+
+    @Bean
+    public Car car2() {
+        Car car = new Car();
+        car.setModel("Honda Civic");
+        return car;
+    }
+
+    @Bean
+    public Car car3() {
+        Car car = new Car();
+        car.setModel("Hyundai Creta");
+        return car;
+    }
 }
